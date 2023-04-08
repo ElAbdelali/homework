562ea54f4f59556a89ed88c231308158fab4ee67
ElAbdelali <eljaouhariabdelali@outlook.com>
2023-04-08 18:46:26 -0500

    Added the days to loop through every day

--- melon-delivery-report/produce_summary.py
+++ melon-delivery-report/produce_summary.py
@@ -1,40 +1,16 @@
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 2")
-the_file = open("um-deliveries-day-2.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-day-3.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+def melon_count_amount(day, filename): #def the function that takes filename as an argument
+    print("Day", day)
+    for line in open(filename): #setting a for loop to loop over each line in the opened file
+        
+        line = line.rstrip() #removing all the whitespace from the document line
+        words = line.split('|') #splitting the words by |
+        
+        
+        melon_type = words[0] #variable initilized to hold the melon type found in words[0]
+        melon_count = words[1] #variable initilized to hold the melon count found in words[1]
+        melon_amount = words[2] #variable initilized to hold the melon cost found in words[2]
+        print(f"Delivered {melon_count} {melon_type}s for total of ${melon_amount}")
+        
+melon_count_amount(1, "um-deliveries-day-1.txt")
+melon_count_amount(2, "um-deliveries-day-2.txt")
+melon_count_amount(3, "um-deliveries-day-3.txt")
